4042c848d16b16221aa808c6c75c0d03d74dfb40
Csaba Marosi <macsaba97@gmail.com>
2025-04-18 20:17:21 +0200

    Data loader added for deepcrack

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,82 +2,75 @@ from albumentations import augmentations, Compose
 from albumentations.pytorch.transforms import ToTensorV2
 import numpy as np
 from torch.utils import data
-from matplotlib import pyplot as plt
+from torch.utils.data import random_split
 import os
 import torch
-def load_data(data_src):
-    imgs, masks = np.load(os.path.join(data_src, 'imgs.npy')), np.load(os.path.join(data_src, 'masks.npy'))
-    
-    print(f'Image array shape: {imgs.shape}')
-    print(f'Mask array shape: {masks.shape}')
-    # visualising first five images
-    fig = plt.figure(figsize = (56, 20))
-    for i in range(5):
-        subplot = fig.add_subplot(2, 5, i + 1)
-        subplot.set_frame_on(True)
-        subplot.xaxis.set_ticks_position('none')
-        subplot.yaxis.set_ticks_position('none')
-        subplot.xaxis.set_ticklabels([])
-        subplot.yaxis.set_ticklabels([])
-        plt.imshow(imgs[i], cmap = 'gray')
-
-        subplot = fig.add_subplot(2, 5, i + 6)
-        subplot.set_frame_on(True)
-        subplot.xaxis.set_ticks_position('none')
-        subplot.yaxis.set_ticks_position('none')
-        subplot.xaxis.set_ticklabels([])
-        subplot.yaxis.set_ticklabels([])
-        plt.imshow(masks[i], cmap = 'gray')
-
-        # creating train-validation split
-
-    # creating train-validation split
-    sources = np.load(os.path.join(data_src, 'sources.npy')) # sources: whether the image comes from the train, validation or test set
-    train_idcs = sources == 'Train'
-
-    train_imgs, train_masks = imgs[train_idcs], masks[train_idcs]
-    val_imgs, val_masks = imgs[~train_idcs], masks[~train_idcs]
-
-    print(f'This gives us {len(train_imgs)} train and {len(val_imgs)} validation samples.')
+from PIL import Image
 
 
-    # keep only 10 images, for testing
-    n_of_images = 10
-    train_imgs = train_imgs[1:n_of_images, :,:]
-    train_masks = train_masks[1:n_of_images, :,:]
-    val_imgs = val_imgs[1:n_of_images, :,:]
-    val_masks = val_masks[1:n_of_images, :,:]
 
+def load_data_deep_crack(image_dir, mask_dir, train_test_lengths, limit_im_nr = None):
+    # Get image list from the given train folder
+    images_list = sorted(os.listdir(image_dir))
+    # do random train test cut on this list
+    train_images_list, val_images_list = random_split(images_list, train_test_lengths)
+
+    print(f'This gives us {len(train_images_list)} train and {len(val_images_list)} validation samples.')
+
+    if limit_im_nr:
+        # keep only limit_im_nr images, for testing
+        train_images_list = list(train_images_list)[0:limit_im_nr]
+        val_images_list = list(val_images_list)[0:limit_im_nr]
+
+    # augmentations
     train_trsfs = Compose([
                        #augmentations.transforms.PadIfNeeded(min_height = 288, min_width = 288, value = 1, border_mode = 0),
-                       augmentations.crops.transforms.RandomCrop(256, 256),
+                       #augmentations.crops.transforms.RandomCrop(256, 256),
                        augmentations.geometric.Rotate(limit = 15, p = 1, border_mode = 0),
                        augmentations.transforms.Normalize(mean = 0.5, std = 0.25),
                        ToTensorV2()
     ])
     val_trsfs = Compose([augmentations.transforms.Normalize(mean = 0.5, std = 0.25), ToTensorV2()])
-    train_ds = CustomDataset(train_imgs, train_masks, train_trsfs)
-    val_ds = CustomDataset(val_imgs, val_masks, val_trsfs)
 
-    train_dl = data.DataLoader(train_ds, batch_size = 16, shuffle = True)
-    val_dl = data.DataLoader(val_ds, batch_size = 16, shuffle = False)
-    return train_dl, val_dl
+    train_dataset = DeepCrackDataset(
+    image_dir=image_dir,
+    mask_dir=mask_dir,
+    image_list = train_images_list,
+    transform=train_trsfs
+    )
+
+    val_dataset = DeepCrackDataset(
+    image_dir=image_dir,
+    mask_dir=mask_dir,
+    image_list = val_images_list,
+    transform=val_trsfs
+    )
 
 
-class CustomDataset(data.Dataset):
-    def __init__(self, imgs, masks, transforms = None):
-        self.imgs = imgs
-        self.masks = masks
-        self.transforms = transforms
+    train_dl = data.DataLoader(train_dataset, batch_size = 16, shuffle = True)
+    val_dl = data.DataLoader(val_dataset, batch_size = 16, shuffle = False)
+    return train_dl, val_dl, train_dataset, val_dataset
+
+class DeepCrackDataset(data.Dataset):
+    def __init__(self, image_dir, mask_dir, image_list, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = image_list
 
     def __len__(self):
-        return len(self.imgs)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        img, mask = self.imgs[idx], self.masks[idx]
-        img = img.reshape((*img.shape, 1))
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        mask_path = os.path.join(self.mask_dir, self.images[idx].replace('.jpg', '.png'))
+
+
+        img = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"))  # grayscale mask
+
         bundle = dict(image = img, mask = mask)
-        if self.transforms:
-            bundle = self.transforms(**bundle)
+        if self.transform:
+            bundle = self.transform(**bundle)
             bundle['mask'] = torch.div(bundle['mask'].unsqueeze(0), 255)
         return bundle
