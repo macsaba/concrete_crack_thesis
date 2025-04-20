d68025a6536ca7f0cd71a47e2c2e6e2c9c5f4f5c
Csaba Marosi <macsaba97@gmail.com>
2025-04-20 10:41:27 +0200

    Training loop validation score and progress added

--- src/training/train.py
+++ src/training/train.py
@@ -1,17 +1,17 @@
 # This implementation is from the deep learning practice
 import torch
 from torchmetrics import ConfusionMatrix
+import matplotlib.pyplot as plt
 
-def train(model, loss_fn, optim, train_ds, val_ds, num_epochs = 1, accum_scale = 1):
+def train(model, loss_fn, optim, train_ds, val_ds, num_epochs = 1, accum_scale = 1, dice_idcs = [], epoch_dice_idcs = [], val_dice_idcs = [], best_model_wts = {}):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.to(device)
 
     # initialise metric calculations
     conf_mtx = torch.zeros((2, 2)) # initial count of confusion matrix entries is 0
     conf_mtx_calc = ConfusionMatrix(task = 'binary')
-    dice_idcs, epoch_dice_idcs, val_dice_idcs = [], [], []
-
-    for _ in range(num_epochs):
+    best_val_score = 0
+    for epoch_idx in range(num_epochs):
         model.train()
         for i, batch in enumerate(train_ds):
             # dataloader returns dict with batched values
@@ -50,4 +50,34 @@ def train(model, loss_fn, optim, train_ds, val_ds, num_epochs = 1, accum_scale =
         dice_idx = (2 * tp + 1) / (2 * tp + fp + fn + 1)
         val_dice_idcs.append(dice_idx.item())
 
+        print('Epoch ',epoch_idx + 1, '. finished.' )
+        print('Validation score: ', val_dice_idcs[-1])
+
+        # save best parameters
+        if val_dice_idcs[-1] > best_val_score:
+            best_model_wts.clear()  # clear previous weights
+            best_model_wts.update(model.state_dict())  # copy new best weights
+            best_val_score = val_dice_idcs[-1]
+
+
+
+def validate(model, loss_fn, optim, train_ds, val_ds, num_epochs = 1, accum_scale = 1):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    conf_mtx_calc = ConfusionMatrix(task = 'binary')
+    dice_idcs, epoch_dice_idcs, val_dice_idcs = [], [], []
+
+    model.eval()
+    for batch in val_ds:
+        batch = {input_type: input.to(device) for input_type, input in batch.items()}
+        with torch.no_grad():
+            pred = model(batch['image'])
+            _ = conf_mtx_calc(pred.to('cpu'), batch['mask'].to('cpu'))
+    tn, fp, fn, tp = conf_mtx_calc.compute().flatten()
+    dice_idx = (2 * tp + 1) / (2 * tp + fp + fn + 1)
+    val_dice_idcs.append(dice_idx.item())
+
     return dice_idcs, epoch_dice_idcs, val_dice_idcs
+
+def test2(x):
+    x[1] = 3
